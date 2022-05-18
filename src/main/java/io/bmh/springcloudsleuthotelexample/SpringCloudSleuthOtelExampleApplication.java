98aaf8687bae06b370a80313b2349a772a907e36
Brandon High <highb@users.noreply.github.com>
2022-05-18 16:35:14 -0700

    Add jib

--- src/main/java/io/bmh/springcloudsleuthotelexample/SpringCloudSleuthOtelExampleApplication.java
+++ src/main/java/io/bmh/springcloudsleuthotelexample/SpringCloudSleuthOtelExampleApplication.java
@@ -2,12 +2,20 @@ package io.bmh.springcloudsleuthotelexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCloudSleuthOtelExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudSleuthOtelExampleApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+		public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+			return String.format("Hello %s!", name);
+	}
 }
