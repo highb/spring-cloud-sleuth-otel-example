d80f5e8910d7f463fc7847fbdbaa251caaff47ba
Brandon High <highb@users.noreply.github.com>
2022-05-18 16:05:15 -0700

    initial spring project generation
    from https://start.spring.io/

---
+++ src/main/java/io/bmh/springcloudsleuthotelexample/SpringCloudSleuthOtelExampleApplication.java
@@ -0,0 +1,13 @@
+package io.bmh.springcloudsleuthotelexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSleuthOtelExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSleuthOtelExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/bmh/springcloudsleuthotelexample/SpringCloudSleuthOtelExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bmh.springcloudsleuthotelexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSleuthOtelExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
